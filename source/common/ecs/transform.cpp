5141bee00aa872750c79112e0dd1175a5c64470d
Mohammed Abdulhady <m.abdulhady00@gmail.com>
2023-04-09 15:04:24 +0200

    feat: requirement 3

--- source/common/ecs/transform.cpp
+++ source/common/ecs/transform.cpp
@@ -2,22 +2,27 @@
 #include "../deserialize-utils.hpp"
 
 #include <glm/gtx/euler_angles.hpp>
-
-namespace our {
-
+namespace our
+{
     // This function computes and returns a matrix that represents this transform
     // Remember that the order of transformations is: Scaling, Rotation then Translation
     // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
-    glm::mat4 Transform::toMat4() const {
-        //TODO: (Req 3) Write this function
-        return glm::mat4(1.0f); 
+    glm::mat4 Transform::toMat4() const
+    {
+        // //TODO: (Req 3) Write this function
+        glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), scale);
+        glm::mat4 rotationMatrix = glm::yawPitchRoll(rotation.y, rotation.x, rotation.z);
+        glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), position);
+        glm::mat4 TRSMatrix = translationMatrix * rotationMatrix * scaleMatrix;
+        return TRSMatrix;
     }
 
-     // Deserializes the entity data and components from a json object
-    void Transform::deserialize(const nlohmann::json& data){
+    // Deserializes the entity data and components from a json object
+    void Transform::deserialize(const nlohmann::json &data)
+    {
         position = data.value("position", position);
         rotation = glm::radians(data.value("rotation", glm::degrees(rotation)));
-        scale    = data.value("scale", scale);
+        scale = data.value("scale", scale);
     }
 
 }
