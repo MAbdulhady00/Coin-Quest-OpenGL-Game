ac1f880576ef259d881ffdfa14d5668a1b21426d
Mohamed Kamal <medoking91@gmail.com>
2023-04-18 19:51:07 +0200

    docs: added functional documentation for materials

--- source/common/material/material.cpp
+++ source/common/material/material.cpp
@@ -6,17 +6,26 @@
 namespace our
 {
 
-    // This function should setup the pipeline state and set the shader to be used
+    /**
+     * @brief Setup the material for rendering.
+     */
     void Material::setup() const
     {
+        // This function should setup the pipeline state and set the shader to be used
         // // TODO: (Req 7) Write this function
         pipelineState.setup();
         shader->use();
     }
 
-    // This function read the material data from a json object
+    /**
+     * @brief Read the material data from a json object.
+     *
+     * @param data json object to read from.
+     */
     void Material::deserialize(const nlohmann::json &data)
     {
+        // This function read the material data from a json object
+
         if (!data.is_object())
             return;
 
@@ -28,30 +37,45 @@ namespace our
         transparent = data.value("transparent", false);
     }
 
-    // This function should call the setup of its parent and
-    // set the "tint" uniform to the value in the member variable tint
+    /**
+     * @brief Setup the tinted material for rendering.
+     */
     void TintedMaterial::setup() const
     {
+
+        // This function should call the setup of its parent and
+        // set the "tint" uniform to the value in the member variable tint
+
         // // TODO: (Req 7) Write this function
         Material::setup();
         shader->set("tint", tint);
     }
 
-    // This function read the material data from a json object
+    /**
+     * @brief Read the material data from a json object.
+     *
+     * @param data json object to read from.
+     */
     void TintedMaterial::deserialize(const nlohmann::json &data)
     {
+        // This function read the material data from a json object
+
         Material::deserialize(data);
         if (!data.is_object())
             return;
         tint = data.value("tint", glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
     }
 
-    // This function should call the setup of its parent and
-    // set the "alphaThreshold" uniform to the value in the member variable alphaThreshold
-    // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex"
+    /**
+     * @brief Setup the texture material for rendering.
+     */
     void TexturedMaterial::setup() const
     {
-        // // TODO: (Req 7) Write this function
+        // This function should call the setup of its parent and
+        // set the "alphaThreshold" uniform to the value in the member variable alphaThreshold
+        // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex"
+
+        // TODO: (Req 7) Write this function
         TintedMaterial::setup();
         shader->set("alphaThreshold", alphaThreshold);
 
@@ -61,9 +85,14 @@ namespace our
         sampler->bind(0); // Bind sampler to texture unit 0
     }
 
-    // This function read the material data from a json object
+    /**
+     * @brief Read the material data from a json object.
+     *
+     * @param data json object to read from.
+     */
     void TexturedMaterial::deserialize(const nlohmann::json &data)
     {
+        // This function read the material data from a json object
         TintedMaterial::deserialize(data);
         if (!data.is_object())
             return;
