5dfb0732c78cde71a5da940f2420d9b727c81f0a
Mohammed Abdulhady <m.abdulhady00@gmail.com>
2023-05-21 19:59:25 +0300

    feat: acceleration movement

--- source/common/components/movement.cpp
+++ source/common/components/movement.cpp
@@ -2,11 +2,19 @@
 #include "../ecs/entity.hpp"
 #include "../deserialize-utils.hpp"
 
-namespace our {
+namespace our
+{
     // Reads linearVelocity & angularVelocity from the given json object
-    void MovementComponent::deserialize(const nlohmann::json& data){
-        if(!data.is_object()) return;
+    void MovementComponent::deserialize(const nlohmann::json &data)
+    {
+        if (!data.is_object())
+            return;
         linearVelocity = data.value("linearVelocity", linearVelocity);
         angularVelocity = glm::radians(data.value("angularVelocity", angularVelocity));
+        linearAcceleration = data.value("linearAcceleration", linearAcceleration);
+        angularAcceleration = glm::radians(data.value("angularAcceleration", angularAcceleration));
+        maxLinearVelocityComponent = data.value("maxLinearVelocityComponent", maxLinearVelocityComponent);
+        maxAngularVelocityComponent = glm::radians(data.value("maxAngularVelocityComponent", maxAngularVelocityComponent));
+        mass = data.value("mass", mass);
     }
 }
--- source/common/components/movement.hpp
+++ source/common/components/movement.hpp
@@ -4,22 +4,31 @@
 
 #include <glm/glm.hpp>
 
-namespace our {
+namespace our
+{
 
     // This component denotes that the MovementSystem will move the owning entity by a certain linear and angular velocity.
     // This component is added as a simple example for how use the ECS framework to implement logic.
     // For more information, see "common/systems/movement.hpp"
     // For a more complex example of how to use the ECS framework, see "free-camera-controller.hpp"
-    class MovementComponent : public Component {
+    class MovementComponent : public Component
+    {
     public:
-        glm::vec3 linearVelocity = {0, 0, 0}; // Each frame, the entity should move as follows: position += linearVelocity * deltaTime 
-        glm::vec3 angularVelocity = {0, 0, 0}; // Each frame, the entity should rotate as follows: rotation += angularVelocity * deltaTime
-        
+        glm::vec3 linearVelocity = {0, 0, 0};      // Each frame, the entity should move as follows: position += linearVelocity * deltaTime
+        glm::vec3 angularVelocity = {0, 0, 0};     // Each frame, the entity should rotate as follows: rotation += angularVelocity * deltaTime
+        glm::vec3 linearAcceleration = {0, 0, 0};  // Each frame, the linearVelocity should change as follows: linearVelocity += linearAcceleration * deltaTime
+        glm::vec3 angularAcceleration = {0, 0, 0}; // Each frame, the angularVelocity should change as follows: angularVelocity += angularAcceleration * deltaTime
+
+        float maxLinearVelocityComponent = 1000.0f;  // The maximum linear velocity that the entity can reach
+        float maxAngularVelocityComponent = 1000.0f; // The maximum angular velocity that the entity can reach
+
+        float mass = 0.0f; // The mass of the entity. Having no mass means that the entity will not be affected by forces.
+
         // The ID of this component type is "Movement"
         static std::string getID() { return "Movement"; }
 
         // Reads linearVelocity & angularVelocity from the given json object
-        void deserialize(const nlohmann::json& data) override;
+        void deserialize(const nlohmann::json &data) override;
     };
 
 }
